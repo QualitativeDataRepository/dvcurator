b213c61b095951a801c823fef651717d319ac324
mccallc <26819445+mccallc@users.noreply.github.com>
2021-11-02 12:57:16 -0400

    check for and integrate with an existing project

--- R/gh_curate.R
+++ R/gh_curate.R
@@ -26,11 +26,42 @@ gh_curate <- function(doi, owner = "QualitativeDataRepository", repo="Project-Cu
   short_title <- get_shortTitle(get_title(data))
   generic_title <- paste(get_author(data), " - ", short_title)
 
-  contact_info <- paste("Contact
-  Name:", get_author(data),"
-  e-mail:", get_email(data),"
-  DV link:", get_url(doi),
-                sep = " ")
+  # Check if there's an existing project and ingest ID numbers.
+  # Create a new one if not.
+  existing_projects <- gh("GET /repos/:owner/:repo/projects", owner=owner, repo=repo)
+  existing_projects <- lapply(existing_projects, function(x) {x[c("name", "id")]})
+  existing_projects <- do.call(rbind.data.frame, existing_projects)
+  if (generic_title %in% existing_projects$name) {
+    projectID <- existing_projects$id[existing_projects$name==generic_title]
+    toDo <- gh("GET /projects/:project/columns", project=projectID)
+    toDo <- do.call(rbind.data.frame, toDo)
+    toDoID <- toDo$id[toDo$name=="To do"]
+  } else {
+    contact_info <- paste0("Contact Name: ", get_author(data), "\n",
+    "e-mail: ", get_email(data), "\n",
+    "DV link: ", get_url(doi))
+
+    project <- gh("POST /repos/:owner/:repo/projects", owner = owner, repo= repo,
+                  name = generic_title, body=contact_info,
+                  .send_headers = c(Accept = "application/vnd.github.inertia-preview+json"))
+
+    projectID <- project$id
+
+    toDo <- gh("POST /projects/:project_id/columns", project_id = projectID,
+               name = "To do",
+               .send_headers = c(Accept = "application/vnd.github.inertia-preview+json"))
+
+    inProgress <- gh("POST /projects/:project_id/columns", project_id = projectID,
+                     name = "In progress",
+                     .send_headers = c(Accept = "application/vnd.github.inertia-preview+json"))
+
+    done <- gh("POST /projects/:project_id/columns", project_id = projectID,
+               name = "Done",
+               .send_headers = c(Accept = "application/vnd.github.inertia-preview+json"))
+
+    toDoID <- toDo$id
+  }
+
 
   # This lapply() creates issues for each specificed ticket and returns a list of their gh objects
   issues <- lapply(tickets, FUN=function(ticket, owner, repo, generic_title) {
@@ -48,30 +79,10 @@ gh_curate <- function(doi, owner = "QualitativeDataRepository", repo="Project-Cu
     message(paste("Created issue", title))
 
     gh("POST /repos/:owner/:repo/issues",
-                owner = owner, repo=repo,
-                title = title, body = body)
+       owner = owner, repo=repo,
+       title = title, body = body)
 
   }, owner, repo, generic_title)
-
-  project <- gh("POST /repos/:owner/:repo/projects", owner = owner, repo= repo,
-                name = generic_title, body=contact_info,
-                .send_headers = c(Accept = "application/vnd.github.inertia-preview+json"))
-
-  projectID <- project$id
-
-  toDo <- gh("POST /projects/:project_id/columns", project_id = projectID,
-             name = "To do",
-             .send_headers = c(Accept = "application/vnd.github.inertia-preview+json"))
-
-  inProgress <- gh("POST /projects/:project_id/columns", project_id = projectID,
-                   name = "In progress",
-                   .send_headers = c(Accept = "application/vnd.github.inertia-preview+json"))
-
-  done <- gh("POST /projects/:project_id/columns", project_id = projectID,
-             name = "Done",
-             .send_headers = c(Accept = "application/vnd.github.inertia-preview+json"))
-
-  toDoID <- toDo$id
 
   for (issue in issues) {
     issueCard <- gh("POST /projects/columns/:column_id/cards", column_id = toDoID, content_id = issue$id,
